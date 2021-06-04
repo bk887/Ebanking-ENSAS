84127670d19f499a1ad3f8ce88ad5f45dfad7083
SAKHR ERROBAI <sakhr.errobai@edu.uca.ma>
2021-06-04 17:02:34 +0100

    consulter solde design

--- src/app/feature/client/consulter-solde/consulter-solde.component.ts
+++ src/app/feature/client/consulter-solde/consulter-solde.component.ts
@@ -12,4 +12,36 @@ export class ConsulterSoldeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  trafficChartData = [
+    {
+      data: [30, 30, 40],
+    }
+  ];
+
+  trafficChartLabels = ["Search Engines", "Direct Click", "Bookmarks Click"];
+
+  trafficChartOptions = {
+    responsive: true,
+    animation: {
+      animateScale: true,
+      animateRotate: true
+    },
+    legend: false,
+  };
+
+  trafficChartColors = [
+    {
+      backgroundColor: [
+        'rgba(177, 148, 250, 1)',
+        'rgba(254, 112, 150, 1)',
+        'rgba(132, 217, 210, 1)'
+      ],
+      borderColor: [
+        'rgba(177, 148, 250, .2)',
+        'rgba(254, 112, 150, .2)',
+        'rgba(132, 217, 210, .2)'
+      ]
+    }
+  ];
+
 }
--- src/app/feature/client/consulter-solde/consulter-solde.module.ts
+++ src/app/feature/client/consulter-solde/consulter-solde.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
+import {NgModule, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConsulterSoldeRoutingModule } from './consulter-solde-routing.module';
 import { ConsulterSoldeComponent } from './consulter-solde.component';
 
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { ChartsModule } from 'ng2-charts';
+import {ChartsDemoModule} from '../../../charts/charts.module';
 
 @NgModule({
   declarations: [ConsulterSoldeComponent],
   imports: [
     CommonModule,
-    ConsulterSoldeRoutingModule
+    ConsulterSoldeRoutingModule,
+    NzStatisticModule,
+    ChartsModule,
+    ChartsDemoModule
   ]
 })
-export class ConsulterSoldeModule { }
+export class ConsulterSoldeModule {
+
+  constructor() { }
+
+}
